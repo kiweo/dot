741462e3a0ba7be5dee8414b7711d35e09c8ab6d
kiweo <206985760+kiweo@users.noreply.github.com>
2025-08-22 18:01:57 +0300

    install.sh: update nvm

--- install.sh
+++ install.sh
@@ -19,7 +19,8 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-\. "$HOME/.nvm/nvm.sh" # use nvm without restarting shell
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 # npm packages
 nvm i --lts
